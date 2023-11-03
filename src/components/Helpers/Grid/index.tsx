ac447d9c9280815b98fc2e3806c229b126f41df1
Ercan Şahin <ercansahin@outlook.com>
2023-11-04 01:34:13 +0300

    dfsdf

--- src/components/Helpers/Grid/index.tsx
+++ src/components/Helpers/Grid/index.tsx
@@ -1,19 +1,19 @@
-import React from "react";
+import React from 'react'
 export default function Grid() {
   return (
-    <div className="fixed inset-0 grid grid-cols-12 gap-5 opacity-10">
-      <div className="bg-red-500" />
-      <div className="bg-red-500" />
-      <div className="bg-red-500" />
-      <div className="bg-red-500" />
-      <div className="bg-red-500" />
-      <div className="bg-red-500" />
-      <div className="bg-red-500" />
-      <div className="bg-red-500" />
-      <div className="bg-red-500" />
-      <div className="bg-red-500" />
-      <div className="bg-red-500" />
-      <div className="bg-red-500" />
+    <div className='fixed inset-0 grid grid-cols-12 gap-5 opacity-10'>
+      <div className='bg-red-500' />
+      <div className='bg-red-500' />
+      <div className='bg-red-500' />
+      <div className='bg-red-500' />
+      <div className='bg-red-500' />
+      <div className='bg-red-500' />
+      <div className='bg-red-500' />
+      <div className='bg-red-500' />
+      <div className='bg-red-500' />
+      <div className='bg-red-500' />
+      <div className='bg-red-500' />
+      <div className='bg-red-500' />
     </div>
-  );
+  )
 }
--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,5 @@
-import {
-  useWindowSize,
-  breakpoints,
-  screens,
-  whichBreakpoint,
-} from "./components/Helpers/Breakpoints";
-import TailwindBreakpoint from "./components/Helpers/TailwindBreakPoint";
-import Grid from "./components/Helpers/Grid";
+import { useWindowSize, breakpoints, screens, whichBreakpoint } from './components/Helpers/Breakpoints'
+import TailwindBreakpoint from './components/Helpers/TailwindBreakPoint'
+import Grid from './components/Helpers/Grid'
 
-export {
-  useWindowSize,
-  breakpoints,
-  screens,
-  whichBreakpoint,
-  TailwindBreakpoint,
-  Grid,
-};
+export { useWindowSize, breakpoints, screens, whichBreakpoint, TailwindBreakpoint, Grid }
